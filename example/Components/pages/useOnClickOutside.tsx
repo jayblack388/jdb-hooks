f5881871ebd78cbb828e5c5bfdd74134d316010b
John Blackwell <jblackwell072588@gmail.com>
2020-04-13 16:46:13 -0700

    fix useOnClickOutside example

--- example/Components/pages/useOnClickOutside.tsx
+++ example/Components/pages/useOnClickOutside.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
 import styled from 'styled-components';
-import { useOnClickOutside as useOnClickOutsideHook } from '../../../dist';
+import {
+  useOnClickOutside as useOnClickOutsideHook,
+  useToggle,
+} from '../../../dist';
 
 const Box = styled.div`
   background: red;
@@ -10,11 +13,19 @@ const Box = styled.div`
 
 export const useOnClickOutside = () => {
   const ref = React.useRef<HTMLDivElement>(null);
+  const [enabled, _, toggleEnabled] = useToggle(true);
   const handler = React.useCallback(() => {
     alert('You clicked outside of the box');
   }, []);
-  useOnClickOutsideHook(ref, handler);
-  return <Box ref={ref}>Click outside the box for an alert!</Box>;
+  useOnClickOutsideHook(ref, handler, enabled);
+  return (
+    <Box ref={ref}>
+      <h4>Click outside the box for an alert!</h4>
+      <button onClick={toggleEnabled}>
+        Example {`${enabled ? 'Enabled' : 'Disabled'}`}
+      </button>
+    </Box>
+  );
 };
 
 export default useOnClickOutside;
