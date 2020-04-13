6d941c5e31c65bcf29a37f42fe7237d64da3e2ac
John Blackwell <jblackwell072588@gmail.com>
2020-04-13 14:10:25 -0700

    Hook/use previous (#4)
    * create hook
    
    * create example

--- example/Components/Layout/index.tsx
+++ example/Components/Layout/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Home, useHover, useToggle } from '../pages';
+import { Home, useHover, usePrevious, useToggle } from '../pages';
 import { Nav } from '../';
 import * as S from './styles';
 
@@ -13,6 +13,7 @@ export const Layout = () => (
         <S.ContentContainer>
           <Switch>
             <Route exact path="/useHover" component={useHover} />
+            <Route exact path="/usePrevious" component={usePrevious} />
             <Route exact path="/useToggle" component={useToggle} />
             <Route path="/" component={Home} />
           </Switch>
--- example/Components/pages/index.ts
+++ example/Components/pages/index.ts
@@ -1,3 +1,4 @@
 export { default as Home } from './Home';
 export { useHover } from './useHover';
+export { usePrevious } from './usePrevious';
 export { useToggle } from './useToggle';
