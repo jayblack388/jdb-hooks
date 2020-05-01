5ae8f90ba5857ef2ad6243be3f39132ceecf85d2
John Blackwell <jblackwell072588@gmail.com>
2020-05-01 11:15:35 -0700

    missing dep

--- src/useDarkMode.ts
+++ src/useDarkMode.ts
@@ -49,7 +49,7 @@ export const useDarkMode = (handler: DarkModeHandler = defaultHandler) => {
 
   useEffect(() => {
     handler(darkModeEnabled);
-  }, [darkModeEnabled]);
+  }, [darkModeEnabled, handler]);
   return [darkModeEnabled, handler, setDarkModeEnabled];
 };
 
